167d9b3dfc009dc11a92ea1b3c6262ffe7d7b3d2
Ahmet Kök <kokahmet@pm.me>
2023-04-25 16:07:01 +0300

    analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
-import '@/styles/globals.css'
-import '@/styles/wordle.css'
+import "@/styles/globals.css";
+import "@/styles/wordle.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   //#202024
-  return <Component {...pageProps} />
+  return( 
+  <>
+  <Component {...pageProps} />
+  <Analytics />
+  </>
+  );
 }
