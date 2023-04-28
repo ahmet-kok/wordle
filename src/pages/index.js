b8436defb0a7d69949aeede6368c36f641758dd3
Ahmet Kök <kokahmet@pm.me>
2023-04-28 05:55:18 +0300

    homepage updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import  Router  from 'next/router'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,11 +11,36 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>
-        Hello World
-      </h1>
-      <button onClick={() => Router.push('/wordle')}>Wordle</button>
-      
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+          <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+            <h1 className="text-6xl font-bold">
+              Welcome to{' '}
+              <Link className="text-blue-600" href="wordle"> 
+               Wordle
+              </Link>
+            </h1>
+            <p className="mt-3 text-2xl">
+              Get started by editing{' '}
+              <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+                pages/index.js
+              </code>
+            </p>
+            <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+              <button
+                onClick={() => Router.push('/wordle')}
+                className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+              >
+              
+    
+                <h3 className="text-2xl font-bold">Wordle &rarr;</h3>
+                <p className="mt-4 text-xl">
+                  lets play wordle
+                </p>
+              </button>
+    </div>
+    </main>
+    </div>
+          
     </>
   )
 }
